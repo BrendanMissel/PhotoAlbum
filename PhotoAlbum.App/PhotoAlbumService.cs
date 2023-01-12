c369e2de650f62edfcb6a3479974029ad61c7304
BrendanMissel <bmissel68@gmail.com>
2023-01-11 18:43:05 -0600

    TEST PASS - GetPhotosByAlbumId_HandlesNull()

--- PhotoAlbum.App/PhotoAlbumService.cs
+++ PhotoAlbum.App/PhotoAlbumService.cs
@@ -1,7 +1,16 @@
 public class PhotoAlbumService 
 {
+    public const string BAD_INPUT_RESPONSE = "Input invalid: Please enter a numeric album id.";
     public IEnumerable<string> GetPhotosByAlbumId(string? userInput)
     {
-        throw new NotImplementedException();
+        List<string> photos = new();
+
+        if (userInput == null) 
+        {
+            photos.Add(BAD_INPUT_RESPONSE);
+            return photos;
+        }
+
+        return photos;
     }
 }
