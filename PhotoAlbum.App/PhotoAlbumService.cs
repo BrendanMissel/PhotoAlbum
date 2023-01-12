c54ac98593f3b9ff29e6223b4fbd6d3ee8880b08
BrendanMissel <bmissel68@gmail.com>
2023-01-11 19:51:58 -0600

    TESTS PASS - REFACTOR - PhotoAlbumService

--- PhotoAlbum.App/PhotoAlbumService.cs
+++ PhotoAlbum.App/PhotoAlbumService.cs
@@ -6,20 +6,39 @@ namespace PhotoAlbum.App
         public IEnumerable<string> GetPhotosByAlbumId(string? userInput)
         {
             List<string> photos = new();
-            int id = -1;
+            
+            photos = ScrubUserInput(ref userInput);
+
+            if (photos.Count > 0)
+            {
+                return photos;
+            }
+
+
+            return photos;
+        }
+
+        /// <summary>
+        /// Given raw user input, ensure it is not null, empty, and a positive integer value. 
+        /// </summary>
+        /// <param name="userInput">Raw user input from the command line.</param>
+        /// <returns>If bad input is detected an error message will be returned to the caller.</returns>
+        private static List<string> ScrubUserInput(ref string? userInput)
+        {
+            List<string> errorMessages = new();
 
             bool isEmpty = userInput == null || userInput == string.Empty;
-            bool isNumeric = int.TryParse(userInput, out id);
+            bool isNumeric = int.TryParse(userInput, out int id);
 
             // Here, I'm assuming the index starts at 1 (rather than 0) as the full dataset shows
             if (isEmpty || (isNumeric && id <= 0) || !isNumeric) 
             {
-                photos.Add(BAD_INPUT_RESPONSE);
-                return photos;
-
+                errorMessages.Add(BAD_INPUT_RESPONSE);
+                return errorMessages;
             }
 
-            return photos;
+            userInput = id.ToString();
+            return errorMessages;
         }
     }
 }
